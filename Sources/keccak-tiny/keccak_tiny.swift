b10e673106dda13b311718fd5d9a0e145a6a2c9f
Kristian Andersen <hello@kristian.co>
2022-03-01 09:40:23 +0100

    Added files

--- Sources/keccak-tiny/keccak_tiny.swift
+++
@@ -1,6 +0,0 @@
-public struct keccak_tiny {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
---
+++ Sources/lib/keccak-tiny/include/keccak-tiny.h
@@ -0,0 +1,26 @@
+//
+//  keccak-tiny.h
+//  SunWallet
+//
+//  Created by Kristian Andersen on 01/03/2022.
+//
+
+#ifndef KECCAK_FIPS202_H
+#define KECCAK_FIPS202_H
+#define __STDC_WANT_LIB_EXT1__ 1
+#include <stdint.h>
+#include <stdlib.h>
+
+#define decshake(bits) \
+int shake##bits(uint8_t*, size_t, const uint8_t*, size_t);
+
+#define deckeccak(bits) \
+int keccak_##bits(uint8_t*, size_t, const uint8_t*, size_t);
+
+decshake(128)
+decshake(256)
+deckeccak(224)
+deckeccak(256)
+deckeccak(384)
+deckeccak(512)
+#endif
